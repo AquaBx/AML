9dd4ef054a59181f9dede6b506b5203d6b1402cc
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-11-23 18:04:33 +0100

    build all ts files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ['src/**/*.ts'],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
   sourcemap: false,
   clean: true,
   outDir: 'dist',
+  entryPoints: ['src/index.ts'],
 });
