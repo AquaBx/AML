173e8036e469b506bb8f0d84a099598f8af81c17
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2024-11-24 03:18:42 +0100

    mutation tests

--- src/fft.ts
+++ src/fft.ts
@@ -1,19 +1,22 @@
 import { Complex } from "./complex";
+import { Float } from "./float";
 
 export class CpxTab {
 
     private tab : Complex[] = []
 
     constructor (n:number){
-        for (let i=0; i<n;i++){
-            this.tab.push(new Complex(0))
-        }
+        this.tab = (new Array<Complex>(n)).fill(new Complex(new Float(0)))
     }
 
-    load (x:number[]){
+    load (x:Float[]){
         for (let i = 0 ; i < x.length ; i++){
             this.tab[i] = new Complex(x[i])
         }
+    }
+
+    toString(){
+        return `[ ${this.tab.join(" ")} ]` 
     }
 
     get(i:number) {
@@ -31,21 +34,21 @@ export class CpxTab {
     conjugue() {
         let newTab = new CpxTab(this.taille())
         for (let i = 0 ; i < this.taille() ; i++){
-            newTab[i] = this.tab[i].conjugue
+            newTab.set(i,this.get(i).conjugue) 
         }
         return newTab
     }
 
-    multiply(c : CpxTab) {
-        let newTab = new CpxTab(this.taille())
-        for (let i = 0 ; i < this.taille() ; i++){
-            newTab[i] = this.tab[i].multiply( c.get(i) )
+    static multiply(c1: CpxTab, c2 : CpxTab) {
+        let newTab = new CpxTab(c1.taille())
+        for (let i = 0 ; i < c1.taille() ; i++){
+            newTab.set(i,Complex.multiply( c1.get(i), c2.get(i) ))
         }
         return newTab
     }
 }
 
-export function combine(c1 : CpxTab , c2 : CpxTab ) : CpxTab {
+function combine(c1 : CpxTab , c2 : CpxTab ) : CpxTab {
 
     if (c1.taille() != c2.taille()){
         throw new Error(`combine: c1 et c2 ne sont pas de même taille, taille c1=${c1.taille()} taille c2=${c2.taille()}`)
@@ -58,44 +61,37 @@ export function combine(c1 : CpxTab , c2 : CpxTab ) : CpxTab {
 
     for (let k = 0; k < m; k++){
 
-        let Wnr = Math.cos( 2 * Math.PI / n * k );
-        let Wni = Math.sin( 2 * Math.PI / n * k );
+        let Wnr = Math.round( Math.cos( 2 * Math.PI / n * k ) * 1**10 ) / 1**10;
+        let Wni = Math.round( Math.sin( 2 * Math.PI / n * k ) * 1**10 ) / 1**10;
 
-        let a1 = Wnr * c2.get(k).real - Wni * c2.get(k).imaginary;
-        let a2 = Wnr * c2.get(k).imaginary + Wni * c2.get(k).real;
+        let Wn = new Complex(new Float(Wnr),new Float(Wni))
 
-        let nk1r = c1.get(k).real + a1
-        let nk1i = c1.get(k).imaginary + a2
+        let a2 = Complex.multiply(Wn,c2.get(k))
 
-        let nk2r = c1.get(k).real - a1
-        let nk2i = c1.get(k).imaginary - a2
-
-        retour.set(k, new Complex(nk1r,nk1i))
-        retour.set(k+n/2, new Complex(nk2r,nk2i))
+        retour.set(k, Complex.add(c1.get(k),a2))
+        retour.set(k+m, Complex.substract(c1.get(k),a2))
     }
-
     return retour;
 }
 
 function  fftRecursive(x:CpxTab) : CpxTab {
-    if (x.taille() % 2 !== 0){
-        throw new Error(`FFT: la taille de x doit être une puissance de 2`)
-    }
-
     let n = x.taille();
 
     if (n==1){return x;}
+    else if (n % 2 !== 0){
+        throw new Error(`FFT: la taille de x doit être une puissance de 2`)
+    }
 
     let Apair = new CpxTab(n/2);
     let Aimpair = new CpxTab(n/2);
 
     // partie pair
     for ( let i = 0; i < n ; i+=2 ){
-        Apair.set(Math.floor(i/2), x.get(i))
+        Apair.set(i/2, x.get(i))
     }
     // partie impair
     for ( let i = 1; i < n ; i+=2 ){
-        Apair.set(Math.floor(i/2), x.get(i))
+        Aimpair.set((i-1)/2, x.get(i))
     }
 
     let yPair = fftRecursive(Apair);
@@ -103,21 +99,25 @@ function  fftRecursive(x:CpxTab) : CpxTab {
 
     return combine(yPair,yImpair);
 }
-
-export function  fft(x : number[]) : CpxTab {
-    let x2 = new CpxTab(Math.ceil(x.length/2)*2)
-    x2.load(x)
-    return fftRecursive(x2);
-}
         
 function  fftInverseRecursive(y:CpxTab) : CpxTab {
-    let taille = new CpxTab(y.taille());
 
-    for (let i=0; i<y.taille() ; i++){
-        taille.set(i, new Complex(1/y.taille()))
+    let t = y.taille()
+    let taille = new CpxTab(t);
+
+    for (let i=0; i<t ; i++){
+        taille.set(i, new Complex(new Float(1,t)))
     }
 
-    return taille.multiply(fftRecursive(y.conjugue()).conjugue());
+    let step1 = y.conjugue()
+    let step2 = fftRecursive(step1)
+    let step3 = step2.conjugue()
+
+    return CpxTab.multiply(taille,step3);
+}	
+
+export function  fft(x : CpxTab) : CpxTab {
+    return fftRecursive(x);
 }
 
 export function fftInverse(x : CpxTab) : CpxTab {
---
+++ src/float.test.ts
@@ -0,0 +1,55 @@
+import { expect, test } from 'vitest'
+import {Float} from './float'
+
+test('6/2 = 3', () => {
+  expect((new Float(6,2)).toNumber()).toBe(3)
+})
+
+test('3/-2 = -3/2', () => {
+  let f1 = new Float(3,-2)
+  let f2 = new Float(-3,2)
+
+  expect(f1).toStrictEqual(f2)
+  expect(f1.toNumber()).toBe(-1.5)
+})
+
+test('0/2 = 0', () => {
+  let f1 = new Float(0,1)
+  let f2 = new Float(0,2)
+
+  expect(f1).toStrictEqual(f2)
+  expect(f1.toNumber()).toBe(0)
+})
+
+test('-1/1 = -1', () => {
+  expect((new Float(-1,1)).toNumber()).toBe(-1)
+})
+
+test('1/0 = Error', () => {
+  expect(() => new Float(1,0)).toThrowError()
+})
+
+test('0/1 . inverse = Error', () => {
+  expect(() => (new Float(0,1)).inverse()).toThrowError("Division by 0")
+})
+
+test('2/7 * 3/5  = 6/35', () => {
+  let f1 = new Float(2,7)
+  let f2 = new Float(3,5)
+  let r = new Float(6,35)
+  expect(Float.multiply(f1,f2)).toStrictEqual(r)
+})
+
+test('2/7 + 3/5  = 31/35', () => {
+  let f1 = new Float(2,7)
+  let f2 = new Float(3,5)
+  let r = new Float(31,35)
+  expect(Float.add(f1,f2)).toStrictEqual(r)
+})
+
+test('toString', () => {
+  let f1 = new Float(-2,7)
+  let f2 = new Float(1)
+  expect(f1.toString()).toBe("-2/7")
+  expect(f2.toString()).toBe("1")
+})
